c9f7927cc7b3fa085a96ee4716ed21095a2134bf
Matthias Maisch <matthias@maisch.me>
2020-08-11 14:08:19 +0200

    rearranged logic

---
+++ README2.md
@@ -0,0 +1,9 @@
+# filebrowser
+
+## Achnowledgements
+
+* Sample images by [Unsplash](https://unsplash.com/)
+
+## Run
+
+`poetry run start`
--- filebrowser/__init__.py
+++ filebrowser/__init__.py
@@ -1,4 +1,17 @@
+from PyQt5 import QtWidgets, uic
+import sys
+
 __version__ = '0.1.0'
+
+class Ui(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(Ui, self).__init__()
+        uic.loadUi('filebrowser/main.ui', self)
+        self.show()
 
 def main():
     print('start filebrowser')
+    app = QtWidgets.QApplication(sys.argv)
+    window = Ui()
+    #Ui()
+    app.exec_()
